d26223d7c6184084166720f41fd3ba2702ffde3b
bittu9975 <99003738+bittu9975@users.noreply.github.com>
2026-06-04 22:33:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,9 +3,29 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
-  
+    const prices = document.querySelectorAll(".prices");
+
+    let total = 0;
+
+    prices.forEach(price => {
+        total += Number(price.textContent);
+    });
+
+    
+    const existingRow = document.getElementById("ans");
+    if (existingRow) {
+        existingRow.remove();
+    }
+
+    const tr = document.createElement("tr");
+    tr.id = "ans";
+
+    const td = document.createElement("td");
+    td.textContent = total;
+
+    tr.appendChild(td);
+
+    document.querySelector("table").appendChild(tr);
 };
 
 getSumBtn.addEventListener("click", getSum);
-
